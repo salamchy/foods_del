1bb95e1364ae1373ccb790be3cd2f88ec866fde1
salam <chaudharysm00@gmail.com>
2024-11-24 20:33:59 +0545

    main layout

---
+++ frontend/src/layout/Main.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom"
+
+const Main = () => {
+  return (
+    <div>
+      <nav>Navbar</nav>
+      <Outlet />
+      <footer>Footer</footer>
+    </div>
+  )
+}
+export default Main
