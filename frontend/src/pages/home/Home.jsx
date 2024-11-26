495041b1885e91ae683c0fdcfa8be73b097397ce
salam <chaudharysm00@gmail.com>
2024-11-26 21:06:42 +0545

    home page

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,9 +1,13 @@
 import Banner from "../../components/Banner"
+import Categories from "../home/Categories"
+import SpecialDishes from "./SpecialDishes"
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <Categories />
+      <SpecialDishes />
     </div>
   )
 }
