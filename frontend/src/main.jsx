57f551f41990c770146d42f4b0b68c5e1ab32d96
salam <chaudharysm00@gmail.com>
2024-12-21 20:34:52 +0545

    main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { Router } from './router/Router.jsx'
+import AuthProvider from './contexts/AuthProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <RouterProvider router={Router} />
+  <AuthProvider>
+    <RouterProvider router={Router} />
+  </AuthProvider>
 )
