d5f7d41a021289e83581a23760bec95d36990862
salam <chaudharysm00@gmail.com>
2024-12-22 21:26:01 +0545

    router

--- frontend/src/router/Router.jsx
+++ frontend/src/router/Router.jsx
@@ -1,6 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
 import Home from "../pages/home/Home";
+import Menu from "../pages/shop/Menu";
+import Signup from "../components/Signup";
+import PrivateRouter from "../PrivateRouter/PrivateRouter";
+import UpdateProfile from "../pages/dashboard/UpdateProfile";
 
 export const Router = createBrowserRouter([
   {
@@ -10,7 +14,19 @@ export const Router = createBrowserRouter([
       {
         path: "/",
         element: <Home />
+      },
+      {
+        path: "/menu",
+        element: <PrivateRouter><Menu /></PrivateRouter>
+      },
+      {
+        path: "/update-profile",
+        element: <UpdateProfile />
       }
     ]
+  },
+  {
+    path: "/signup",
+    element: <Signup />
   }
 ])
